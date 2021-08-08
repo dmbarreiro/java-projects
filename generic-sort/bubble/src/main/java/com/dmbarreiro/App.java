81209cb9537ef06cb4c819cf6877317edd0ef4d3
dmbarreiro <5133990+dmbarreiro@users.noreply.github.com>
2021-08-08 23:59:31 +0200

    Initial commit
    Basic project structure

---
+++ generic-sort/bubble/src/main/java/com/dmbarreiro/App.java
@@ -0,0 +1,13 @@
+package com.dmbarreiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
